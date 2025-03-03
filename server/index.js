f0887b1fc80bcb29faf4db9539b28d50a6065a9e
kientvhe172514 <kientvhe172514@fpt.edu.vn>
2025-03-04 00:10:07 +0700

    can run

---
+++ server/index.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require('path');
+const cors = require("cors");
+const connectDB = require("./config/db");
+const { secret } = require("./config/secret");
+const PORT = secret.port || 7000;
+const morgan = require('morgan')
+// error handler
+const globalErrorHandler = require("./middleware/global-error-handler");
+// routes
+const userRoutes = require("./routes/user.routes");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// connect database
+connectDB();
+
+app.use("/api/user", userRoutes);
+
+
+// root route
+app.get("/", (req, res) => res.send("Apps worked successfully"));
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+// global error handler
+app.use(globalErrorHandler);
+//* handle not found
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
+
+module.exports = app;
