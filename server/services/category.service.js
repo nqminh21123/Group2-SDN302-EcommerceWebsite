20c78712eae198fbda08c90011386e81bbe8a893
HuyTQ <huytqhe170798@fpt.edu.vn>
2025-03-18 00:09:49 +0700

    Update category.service.js

--- server/services/category.service.js
+++ server/services/category.service.js
@@ -1,60 +1,63 @@
-const ApiError = require('../errors/api-error');
-const Category = require('../model/Category');
-const Products = require('../model/Products');
+const _ = require("mongoose-sequence");
+const ApiError = require("../errors/api-error");
+const Category = require("../model/Category");
+const Products = require("../model/Products");
 
 // create category service
 exports.createCategoryService = async (data) => {
   const category = await Category.create(data);
   return category;
-}
+};
 
 // create all category service
 exports.addAllCategoryService = async (data) => {
-  await Category.deleteMany()
+  await Category.deleteMany();
   const category = await Category.insertMany(data);
   return category;
-}
+};
 
 // get all show category service
 exports.getShowCategoryServices = async () => {
-  const category = await Category.find({status:'Show'}).populate('products');
+  const category = await Category.find({ status: "Show" }).populate("products");
   return category;
-}
+};
 
-// get all category 
+// get all category
 exports.getAllCategoryServices = async () => {
-  const category = await Category.find({})
+  const category = await Category.find({});
   return category;
-}
+};
 
 // get type of category service
 exports.getCategoryTypeService = async (param) => {
-  const categories = await Category.find({productType:param}).populate('products');
+  const categories = await Category.find({ productType: param }).populate(
+    "products"
+  );
   return categories;
-}
+};
 
 // get type of category service
 exports.deleteCategoryService = async (id) => {
   const result = await Category.findByIdAndDelete(id);
   return result;
-}
+};
 
 // update category
-exports.updateCategoryService = async (id,payload) => {
-  const isExist = await Category.findOne({ _id:id })
+exports.updateCategoryService = async (id, payload) => {
+  const isExist = await Category.findOne({ _id: id });
 
   if (!isExist) {
-    throw new ApiError(404, 'Category not found !')
+    throw new ApiError(404, "Category not found !");
   }
 
-  const result = await Category.findOneAndUpdate({ _id:id }, payload, {
+  const result = await Category.findOneAndUpdate({ _id: id }, payload, {
     new: true,
-  })
-  return result
-}
+  });
+  return result;
+};
 
 // get single category
 exports.getSingleCategoryService = async (id) => {
-  const result = await Category.findById(id);
+  const result = await Category.findOne({ _id: id });
   return result;
-}
+};
